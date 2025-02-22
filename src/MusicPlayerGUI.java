9e1434462d6d1da91fd4828e18cbc5e397a148cd
AubsFSC <morraj58@farmingdale.edu>
2025-02-22 17:29:39 -0500

    GUI song image, title, artist and playback slider displayed in gui

--- src/MusicPlayerGUI.java
+++ src/MusicPlayerGUI.java
@@ -1,6 +1,14 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class MusicPlayerGUI extends JFrame {
+
+    public static final Color FRAME_COLOR = Color.BLUE;
+    public static final Color TEXT_COLOR = Color.WHITE;
+
     public MusicPlayerGUI(){
         // calls JFrame constructor to configure GUI and set Title Header
         super("Music Player");
@@ -18,12 +26,45 @@ public class MusicPlayerGUI extends JFrame {
         setResizable(false);
         setLayout(null);
 
+        //change colors
+        getContentPane().setBackground(FRAME_COLOR);
+
+
         addGuiComponents();
 
     }
 
     private void addGuiComponents(){
+
         addToolbar();
+
+        //load album img
+        JLabel songImage = new JLabel(loadImage("src/assets/record.png"));
+        songImage.setBounds(0, 50, getWidth() - 20, 225);
+        add(songImage);
+
+        //show song title
+        JLabel songTitle = new JLabel("Song Title");
+        songTitle.setBounds(0, 285, getWidth() - 10, 30);
+        songTitle.setFont(new Font("Arial", Font.BOLD, 22));
+        songTitle.setForeground(TEXT_COLOR);
+        songTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        add(songTitle);
+
+        //show song artist
+        JLabel songArtist = new JLabel("Artist");
+        songArtist.setBounds(0, 315, getWidth() - 10, 30);
+        songArtist.setFont(new Font("Arial", Font.BOLD, 18));
+        songArtist.setForeground(TEXT_COLOR);
+        songArtist.setHorizontalAlignment(SwingConstants.CENTER);
+        add(songArtist);
+
+        //show playback slider
+        JSlider playbackSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 0);
+        playbackSlider.setBounds(getWidth()/2 - 300/2, 365, 300, 40);
+        playbackSlider.setBackground(null);
+        add(playbackSlider);
+
     }
 
     private void addToolbar(){
@@ -58,6 +99,20 @@ public class MusicPlayerGUI extends JFrame {
 
 
         add(toolbar);
+    }
+
+    private ImageIcon loadImage(String imagePath){
+        try{
+            //read the image file
+            BufferedImage image = ImageIO.read(new File(imagePath));
+
+            return new ImageIcon(image);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //if cant find img
+        return null;
     }
 
 }
